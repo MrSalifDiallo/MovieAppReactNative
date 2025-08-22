5f6b03b532bbc5d6d12a461e572d8dc7955cc29a
MrDialloDev <salifdiallo@esp.sn>
2025-08-22 21:04:28 +0000

    Setup Tailwind with Adding newfile by follow Guide Start of Native Wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
 import { Text, View } from "react-native";
-
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome</Text>
+    <View className="flex-1 justify-center items-center" >
+      <Text className="text-5xl text-primary font-bold">Welcome to your Movie App!</Text>
     </View>
   );
 }
