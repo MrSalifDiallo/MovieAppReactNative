cabf230711c36dacca93a21ab5488a8ef191c94c
MrDialloDev <salifdiallo@esp.sn>
2025-08-23 04:04:08 +0000

    Custom Hook for fetching Movies and Display

---
+++ services/useFetch.ts
@@ -0,0 +1,41 @@
+//fetchPopularMovie
+//fetchMovieDetails whatever we can use for a function
+
+import { useEffect, useState } from "react"
+
+const useFetch=<T>(fetchFunction:()=>Promise<T>,autoFetch=true)=>{
+    const [data,setData]=useState<T|null>(null)
+    const [loading,setLoading]=useState(false)
+    const [error,setError]=useState<Error | null>(null)
+
+
+    const fetchData= async () => {
+        try {
+            setLoading(true)
+            setError(null)
+            const result =await fetchFunction()
+            setData(result)
+        } 
+        catch (error) {
+            setError(error instanceof Error? error :new Error('An Error Occured'))
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    const reset =()=>{
+        setData(null)
+        setError(null)
+        setLoading(false)
+    }
+
+    useEffect(()=>{
+        if (autoFetch) {
+            fetchData()
+        }
+    },[])
+    return {data,loading,error,refetch:fetchData,reset}
+}
+
+export default useFetch
